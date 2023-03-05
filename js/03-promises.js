cbc0aaa39b92f83e4c3b257cd45ba972577c0673
OlegArt1 <113111922+OlegArt1@users.noreply.github.com>
2023-03-05 16:30:49 +0300

    Added files

---
+++ js/03-promises.js
@@ -0,0 +1,74 @@
+
+const body = document.querySelector("body");
+const formElement = document.querySelector(".form");
+const labelElement = document.querySelector("label");
+
+const inputFirstElement = formElement.firstElementChild.firstElementChild;
+const inputMiddleElement = formElement.firstElementChild.nextElementSibling.firstElementChild;
+const inputLastElement = formElement.firstElementChild.nextElementSibling.nextElementSibling.firstElementChild;
+const buttonElement = formElement.lastElementChild;
+
+formElement.style.marginTop = '20px';
+formElement.style.marginLeft = '20px';
+
+function createPromise (position, delay)
+{
+    const shouldResolve = Math.random() > 0.3;
+    const promise_result = new Promise ((resolve, reject) =>
+    {
+        setTimeout(() =>
+        {
+            if (shouldResolve)
+            {
+                resolve({ position, delay });
+            }
+            else
+            {
+                reject({ position, delay });
+            }
+
+        }, delay);
+    });
+    return promise_result;
+}
+buttonElement.addEventListener('click', (e) =>
+{
+    e.preventDefault();
+
+    let first_delay = Number.parseInt(inputFirstElement.value);
+    let delay_step = Number.parseInt(inputMiddleElement.value);
+    let amount_ = Number.parseInt(inputLastElement.value);
+
+    if (inputFirstElement.value !== '' && inputMiddleElement.value !== '' && inputLastElement.value !== '')
+    {
+        if (first_delay > 0 && delay_step > 0 && amount_ > 0)
+        {
+            console.log("\nStart promise!");
+            let i = 0;
+        
+            while (i < amount_)
+            {   
+                createPromise(amount_, delay_step)
+                    .then(({ position, delay }) =>
+                    {
+                        console.log(`\nFulfilled promise ${position} in ${delay} ms;`);
+                    })
+                    .catch(({ position, delay }) =>
+                    {
+                        console.log(`\nRejected promise ${position} in ${delay} ms;`);
+                    });
+                i += 1;
+            }
+            console.log("\nDelay first = " + first_delay + " ms; " + "Delay step = " + delay_step + " ms; " + "Amount = " + amount_ + "; ");
+        }
+        else
+        {
+            console.log("\nОшибка! Введите правильные значения!");
+        }
+    }
+    else
+    {
+        console.log("\nОшибка! Введите правильные значения!");
+    }
+    formElement.reset();
+});
